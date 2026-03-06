7264d47c8712fa76727bcfc7a3272eb3998e7e18
Daniel Oliveira <ca.daniel@outlook.com>
2026-03-06 10:38:18 -0300

    ♻️ refactor: rename variables

--- services/table_service.py
+++ services/table_service.py
@@ -1,5 +1,10 @@
+import matplotlib
+matplotlib.use("Agg")
+
 import matplotlib.pyplot as plt
+import pathlib as pl
 import pandas as pd
+
 from config import OUTPUT_PATH
 
 def generate_table(df: pd.DataFrame, office_name: str)-> None:
@@ -9,7 +14,7 @@ def generate_table(df: pd.DataFrame, office_name: str)-> None:
         df (pd.DataFrame): A DataFrame containing the data to be included in the report.
         office_name (str): The name of the office for which the report is generated.
     """
-    fig, ax = plt.subplots(figsize=(14, 5))
+    fig, ax = plt.subplots(figsize=(12, 8))
     ax.axis("off")
 
     table = ax.table(
@@ -23,34 +28,37 @@ def generate_table(df: pd.DataFrame, office_name: str)-> None:
     table.set_fontsize(12)
     table.scale(1.1, 1.6)
 
-    n_linhas = len(df)
-    n_colunas = len(df.columns)
+    row_number = len(df)
+    column_number = len(df.columns)
 
-    verde_escuro = "#1B5E20"
-    verde_claro = "#E8F5E9"
+    dark_green = "#1B5E20"
+    light_green = "#E8F5E9"
 
     for (row, col), cell in table.get_celld().items():
 
         if row == 0:
-            cell.set_facecolor(verde_escuro)
+            cell.set_facecolor(dark_green)
             cell.set_text_props(color="white", weight="bold", ha='center')
 
-        elif row == n_linhas:
-            cell.set_facecolor(verde_escuro)
+        elif row == row_number:
+            cell.set_facecolor(dark_green)
             cell.set_text_props(color="white", weight="bold")
 
         elif row % 2 == 0:
-            cell.set_facecolor(verde_claro)
+            cell.set_facecolor(light_green)
 
-        if col == n_colunas - 1:
+        if col == column_number - 1:
             cell._text.set_ha('right')
 
         cell.set_linewidth(0)
 
-    table.auto_set_column_width(col=list(range(n_colunas)))
+    table.auto_set_column_width(col=list(range(column_number)))
+
+    output_dir = pl.Path(OUTPUT_PATH)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     plt.savefig(
-        f"{OUTPUT_PATH}/{office_name.replace(' ', '_')}.png",
+        output_dir / f"{office_name.replace(' ', '_')}.png",
         bbox_inches='tight',
         dpi=300
     )
