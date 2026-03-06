b4ee70cc8238f9e13e1149463b44044d98cebf1b
Daniel Oliveira <ca.daniel@outlook.com>
2026-03-06 10:38:58 -0300

    ✨ feat: add thread process

--- __main__.py
+++ __main__.py
@@ -1,16 +1,28 @@
-from services.query_service import get_offices, get_office_data
-from services.table_service import generate_table
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from zipfile import ZipFile
+
+from services.query_service import get_offices
+from processors.process_office import process_office
+from config import OUTPUT_PATH
+
+output_dir = Path(OUTPUT_PATH)
+output_dir.mkdir(parents=True, exist_ok=True)
+
+zip_path = Path("/tmp/relatorios.zip")
 
 def main():
     offices = get_offices()
 
-    for office in offices:
-        df = get_office_data(office.office_id)
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        executor.map(process_office, offices)
 
-        if df.empty:
-            continue
+    with ZipFile(zip_path, "w") as zipf:
+        for file in output_dir.iterdir():
+            if file.is_file():
+                arcname = f"escritorios/{file.name}"
 
-        generate_table(df, office.office_name)
+                zipf.write(file, arcname=arcname)
 
 if __name__ == "__main__":
     main()
