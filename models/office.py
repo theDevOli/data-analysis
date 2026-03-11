e41fee0f8df22947ba98114c6da42e9784eae4e0
Daniel Oliveira <ca.daniel@outlook.com>
2026-03-11 09:29:08 -0300

    ♻️ refactor: update office model

--- models/office.py
+++ models/office.py
@@ -8,6 +8,8 @@ class Office:
     Attributes:
         office_id (str): Unique identifier of the office.
         office_name (str): Name of the office.
+        city (str): City where the office is located.
     """
     office_id: str
     office_name: str
+    city: str
